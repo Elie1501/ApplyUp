fedd746c1f5c3c8a35b98a63f4671f01ca11a74a
Michaël Duthilleul <michael.duthilleul@gmail.com>
2026-01-06 15:21:36 +0100

    Add job & email in ApplyForm

--- src/components/ApplyForm.tsx
+++ src/components/ApplyForm.tsx
@@ -1,27 +1,64 @@
 import { useState } from "react";
 
+type Job = {
+  id: 'front' | 'back';
+  title: string;
+};
+
+const JOBS: Job[] = [
+  { id: "front", title: "Développeur Frontend" },
+  { id: "back", title: "Développeur Backend" },
+];
+
 export const ApplyForm = () => {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [job, setJob] = useState<Job["id"]>("front");
 
   return (
     <div>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        alert(name)
-      }}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          alert(`${name}, ${email}, ${job}`);
+        }}
+        className="flex flex-col gap-4"
+      >
         <input
           name="name"
+          required
           type="text"
           placeholder="Votre nom"
           className="input"
+          value={name}
           onChange={(e) => {
             setName(e.target.value);
           }}
         />
+        {!name && <p className="text-red-500">Le nom est requis</p>}
+        <input
+          name="email"
+          type="email"
+          required
+          placeholder="Votre email"
+          className="input"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <select defaultValue="front" className="select" value={job} onChange={(e) => setJob(e.target.value as Job["id"])}>
+          {JOBS.map((job) => (
+            <option key={job.id} value={job.id}>
+              {job.title}
+            </option>
+          ))}
+        </select>
+        <textarea className="textarea" placeholder="Description"></textarea>
         <button className="btn btn-success" type="submit">
           Envoyer
         </button>
       </form>
     </div>
   );
-}
+};
